12563a05c1b2d1de6060ab4fa0ec0e82f766ff34
iditchev <iditchev@my.wctc.edu>
2021-03-25 18:21:33 -0500

    Add Search option

--- Program.cs
+++ Program.cs
@@ -15,44 +15,57 @@ namespace Ticketing_System
                     do
                     {
                         // ask a question
-                        Console.WriteLine("Enter a Ticket (Y/N)?");
+                        Console.WriteLine("1) Enter a Ticket");
+                        Console.WriteLine("2) Search for Ticket");
+                        Console.WriteLine("Anything else to Exit");
                         // input the response
-                        resp = Console.ReadLine().ToUpper();
-                        if (resp != "Y") { break; }
+                        resp = Console.ReadLine();
+                        if (resp == "1") 
 
-                        
-                       
+                        {
                         Console.WriteLine("Choose the ticket type:");
                         Console.WriteLine("1. Bug/Defect");
                         Console.WriteLine("2. Enhancement");
                         Console.WriteLine("3. Task");
                         
-                        string TicketType = Console.ReadLine();
-                        if (TicketType == "1")
-                        {
+                            string TicketType = Console.ReadLine();
+                            if (TicketType == "1")
+                            {
                             Bug newbug = new Bug();
                             newbug.AskUser();
                             newbug.AddTickettoFile(Bugfile);
-                        }
+                            }
 
-                        if (TicketType == "2")
-                        {
+                            if (TicketType == "2")
+                            {
                             Enhancement newenhancement = new Enhancement();
                             newenhancement.AskUser();
                             newenhancement.AddTickettoFile(EnchancementFile);
                         
-                        }
+                            }
                         
-                        if (TicketType == "3")
-                        {
+                            if (TicketType == "3")
+                            {
                             Task newtask = new Task ();
                             newtask.AskUser();
                             newtask.AddTickettoFile(TaskFile);
+                            }
                         }
+                       
+                     else if (resp == "2")
+                      {
+                          Console.WriteLine("Search on:");
+                          Console.WriteLine("1) Status");
+                          Console.WriteLine("2) Priority");
+                          Console.WriteLine("3) Submitter");
+
+                          String search = Console.ReadLine().ToLower();
+                          
+                      }
 
                         
                         
-                    } while (resp == "Y");
+                    } while (resp == "1"|| resp == "2");
 
                    
                 }
