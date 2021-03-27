107dcbf73279af3d43962268a35448a66e151ce0
iditchev <iditchev@my.wctc.edu>
2021-03-27 16:54:09 -0500

    Bugfile Class

--- TicketFile.cs
+++ TicketFile.cs
@@ -11,7 +11,7 @@ namespace Ticketing_System
         public string filePath { get; set; }
         public List<Ticket> Tickets { get; set; }
 
-        public override void TicketFile (string ticketfilepath)
+        public virtual void FileRead (string ticketfilepath)
         {
             filePath = ticketfilepath;
             Tickets = new List<Ticket>();
@@ -40,10 +40,39 @@ namespace Ticketing_System
                 sr.Close();
             }  
         } 
-    }         
+             
     public class BugFile : TicketFile
     {
+            public override void FileRead (string ticketfilepath)
+        {
+            filePath = ticketfilepath;
+            Tickets = new List<Ticket>();
 
+             
+            
+                StreamReader sr = new StreamReader(filePath);
+                while (!sr.EndOfStream)
+                {
+                    // create instance of Movie class
+                    Bug ticket = new Bug();
+                    string line = sr.ReadLine();
+
+                    string[] ticketDetails = line.Split(',');
+                        ticket.ticketID = ticketDetails[0];
+                        ticket.summary = ticketDetails[1];
+                        ticket.status = ticketDetails[2];
+                        ticket.priority = ticketDetails[3];
+                        ticket.submitter = ticketDetails[4];
+                        ticket.assigned = ticketDetails[5];
+                        ticket.watching = ticketDetails[6].Split('|').ToArray();
+                        ticket.Severity = ticketDetails[7];
+
+                        
+                        Tickets.Add(ticket);
+
+                }
+                sr.Close();
+            }  
     }
 
     public class EnchancementFile : TicketFile
