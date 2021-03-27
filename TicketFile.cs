8d9e3820669f534f092094790639a83f6714fa61
iditchev <iditchev@my.wctc.edu>
2021-03-27 16:58:13 -0500

    EnhancementFile Class

--- TicketFile.cs
+++ TicketFile.cs
@@ -77,7 +77,39 @@ namespace Ticketing_System
 
     public class EnchancementFile : TicketFile
     {
+        public override void FileRead (string ticketfilepath)
+        {
+            filePath = ticketfilepath;
+            Tickets = new List<Ticket>();
 
+             
+            
+                StreamReader sr = new StreamReader(filePath);
+                while (!sr.EndOfStream)
+                {
+                    // create instance of Movie class
+                    Enhancement ticket = new Enhancement();
+                    string line = sr.ReadLine();
+
+                    string[] ticketDetails = line.Split(',');
+                        ticket.ticketID = ticketDetails[0];
+                        ticket.summary = ticketDetails[1];
+                        ticket.status = ticketDetails[2];
+                        ticket.priority = ticketDetails[3];
+                        ticket.submitter = ticketDetails[4];
+                        ticket.assigned = ticketDetails[5];
+                        ticket.watching = ticketDetails[6].Split('|').ToArray();
+                        ticket.Software = ticketDetails[7];
+                        ticket.Cost = ticketDetails[8];
+                        ticket.Reason = ticketDetails [9];
+                        ticket.Estimate = ticketDetails [10];
+
+                        
+                        Tickets.Add(ticket);
+
+                }
+                sr.Close();
+            }  
     }
 
     public class TaskFile : TicketFile
