58cdc61a1b43f0cf2e9c68eaab70c1ea03ac2a22
iditchev <iditchev@my.wctc.edu>
2021-03-27 16:59:58 -0500

    TaskFile Class

--- TicketFile.cs
+++ TicketFile.cs
@@ -114,6 +114,36 @@ namespace Ticketing_System
 
     public class TaskFile : TicketFile
     {
+        public override void FileRead (string ticketfilepath)
+        {
+            filePath = ticketfilepath;
+            Tickets = new List<Ticket>();
 
+             
+            
+                StreamReader sr = new StreamReader(filePath);
+                while (!sr.EndOfStream)
+                {
+                    // create instance of Movie class
+                    Task ticket = new Task();
+                    string line = sr.ReadLine();
+
+                    string[] ticketDetails = line.Split(',');
+                        ticket.ticketID = ticketDetails[0];
+                        ticket.summary = ticketDetails[1];
+                        ticket.status = ticketDetails[2];
+                        ticket.priority = ticketDetails[3];
+                        ticket.submitter = ticketDetails[4];
+                        ticket.assigned = ticketDetails[5];
+                        ticket.watching = ticketDetails[6].Split('|').ToArray();
+                        ticket.ProjectName = ticketDetails[7];
+                        ticket.DueDate = ticketDetails [8];
+
+                        
+                        Tickets.Add(ticket);
+
+                }
+                sr.Close();
+            }  
     }
 }  
